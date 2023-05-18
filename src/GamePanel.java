e795e8a0c8fb4d43d84745eccabb357d9fe6d1b6
amolavi23 <aryonmolavi@gmail.com>
2023-05-18 10:49:08 -0600

    Added Tail

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -15,6 +15,8 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
     static final int DELAY = 75;
+    int counter = 0;
+
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
     int bodyParts = 6;
@@ -55,10 +57,10 @@ public class GamePanel extends JPanel implements ActionListener {
 
             try {
 
-                String path= "snake/images/Apples.jpg";
-                BufferedImage imageOrignal=ImageIO.read(new File(path));
-                Image image=imageOrignal.getScaledInstance(UNIT_SIZE,UNIT_SIZE,Image.SCALE_DEFAULT);
-                g.drawImage(image,applesX,applesY,null);
+                String path = "snake/images/Apples.jpg";
+                BufferedImage imageOrignal = ImageIO.read(new File(path));
+                Image image = imageOrignal.getScaledInstance(UNIT_SIZE, UNIT_SIZE, Image.SCALE_DEFAULT);
+                g.drawImage(image, applesX, applesY, null);
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -67,22 +69,34 @@ public class GamePanel extends JPanel implements ActionListener {
             for (int i = 0; i < bodyParts; i++) {
                 if (i == 0) {
                     try {
-                        String path="snake/images/SnakeHead"+direction+".jpg";
-                        BufferedImage imageOrignal=ImageIO.read(new File(path));
-                        Image image=imageOrignal.getScaledInstance(UNIT_SIZE,UNIT_SIZE,Image.SCALE_DEFAULT);
-                        g.drawImage(image,x[i],y[i],null);
+                        String path = "snake/images/SnakeHead" + direction + ".jpg";
+                        BufferedImage imageOrignal = ImageIO.read(new File(path));
+                        Image image = imageOrignal.getScaledInstance(UNIT_SIZE, UNIT_SIZE, Image.SCALE_DEFAULT);
+                        g.drawImage(image, x[i], y[i], null);
 
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
 
+                } else if (i == bodyParts - 1) {
+
+                    try {
+
+                        String path = "snake/images/SnakeTail" + getTailDirection() + ".jpg";
+                        BufferedImage imageOrignal = ImageIO.read(new File(path));
+                        Image image = imageOrignal.getScaledInstance(UNIT_SIZE, UNIT_SIZE, Image.SCALE_DEFAULT);
+                        g.drawImage(image, x[i], y[i], null);
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 } else {
                     g.setColor(new Color(45, 180, 0));
                     g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-                    g.setColor(Color.WHITE);
-                    g.fillRect(0,0,UNIT_SIZE,UNIT_SIZE);
 
                 }
+                g.setColor(Color.WHITE);
+                g.fillRect(0, 0, UNIT_SIZE, UNIT_SIZE);
             }
         } else {
             gameOver(g);
@@ -102,6 +116,7 @@ public class GamePanel extends JPanel implements ActionListener {
         switch (direction) {
             case 'U':
                 y[0] = y[0] - UNIT_SIZE;
+
                 break;
             case 'D':
                 y[0] = y[0] + UNIT_SIZE;
@@ -118,7 +133,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void checkApple() {
         if ((x[0] == applesX) && (y[0] == applesY)) {
-            bodyParts+=3;
+            bodyParts += 3;
             applesEaten++;
             newApple();
         }
@@ -150,55 +165,72 @@ public class GamePanel extends JPanel implements ActionListener {
     public void gameOver(Graphics g) {
         //Score
         try {
-            String path= "snake/images/GameOver.jpeg";
-            BufferedImage imageOrignal=ImageIO.read(new File(path));
-            Image image=imageOrignal.getScaledInstance(SCREEN_WIDTH,SCREEN_HEIGHT,Image.SCALE_DEFAULT);
-            g.drawImage(image,0,0,null);
+            String path = "snake/images/GameOver.jpeg";
+            BufferedImage imageOrignal = ImageIO.read(new File(path));
+            Image image = imageOrignal.getScaledInstance(SCREEN_WIDTH, SCREEN_HEIGHT, Image.SCALE_DEFAULT);
+            g.drawImage(image, 0, 0, null);
             g.setColor(Color.RED);
-            Font f=new Font("Serif", Font.BOLD,50);
+            Font f = new Font("Serif", Font.BOLD, 50);
             g.setFont(f);
-            g.drawString("Score: "+applesEaten, 225,500);
-            g.drawString("Score: "+applesEaten, 225,500);
+            g.drawString("Score: " + applesEaten, 225, 500);
+            g.drawString("Score: " + applesEaten, 225, 500);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void actionPerformed(ActionEvent e) {
-        if (running) {
-            move();
-            checkApple();
-            checkCollisions();
+    public char getTailDirection() {
+        if (x[bodyParts - 2] == x[bodyParts - 1] + UNIT_SIZE) {
+            return 'R';
+        } else if (x[bodyParts - 2] == x[bodyParts - 1] - UNIT_SIZE) {
+            return 'L';
+        } else if (y[bodyParts - 2] == y[bodyParts - 1] - UNIT_SIZE) {
+            return 'U';
+        } else {
+            return 'D';
         }
-        repaint();
     }
+        public void actionPerformed (ActionEvent e){
+            if (running) {
+                move();
+                checkApple();
+                checkCollisions();
+            }
+            repaint();
+        }
 
-    public class MyKeyAdapter extends KeyAdapter {
-        @Override
-        public void keyPressed(KeyEvent e) {
-            switch (e.getKeyCode()) {
-                case KeyEvent.VK_LEFT:
-                    if (direction != 'R') {
-                        direction = 'L';
-                    }
-                    break;
-                case KeyEvent.VK_RIGHT:
-                    if (direction != 'L') {
-                        direction = 'R';
-                    }
-                    break;
-                case KeyEvent.VK_UP:
-                    if (direction != 'D') {
-                        direction = 'U';
-                    }
-                    break;
-                case KeyEvent.VK_DOWN:
-                    if (direction != 'U') {
-                        direction = 'D';
-                    }
-                    break;
+
+        public class MyKeyAdapter extends KeyAdapter {
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        if (direction != 'R') {
+                            direction = 'L';
+                        }
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        if (direction != 'L') {
+                            direction = 'R';
+                            counter++;
+                        }
+                        break;
+                    case KeyEvent.VK_UP:
+                        if (direction != 'D') {
+                            direction = 'U';
+                            counter++;
+                        }
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        if (direction != 'U') {
+                            direction = 'D';
+                            counter++;
+                        }
+                        break;
+                }
             }
         }
     }
-}
+
